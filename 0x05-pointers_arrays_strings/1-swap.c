ce90571a027643e381b25f14f83231aee832eb91
king-luminous <albashir97@gmail.comgit config --global user.email albashir97@gmail.com>
2022-10-25 06:26:41 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swapping the value of two integer
+ * @a: int a
+ * @b: int b
+ * Return: void
+ */
+
+void swap_int(int *a, int *b);
+{
+	int q;
+
+	q = *a;
+	*a = *b;
+	*b = q;
+}
