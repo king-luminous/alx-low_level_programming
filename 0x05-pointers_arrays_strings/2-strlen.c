3cf38796b9850554cf9b653a1d62e51de1d1c78b
king-luminous <albashir97@gmail.comgit config --global user.email albashir97@gmail.com>
2022-10-25 13:34:09 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a stting
+ * @s: string
+ * Return: lengths
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len)
+}
