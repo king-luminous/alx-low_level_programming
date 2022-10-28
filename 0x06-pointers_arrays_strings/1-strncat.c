4fc49946ce6f42d9475d69a19db1a5fbfb1c7891
king-luminous <albashir97@gmail.comgit config --global user.email albashir97@gmail.com>
2022-10-28 14:07:45 -0700

    Task 001

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _strncat - joins two strings with n bytes
@@ -13,17 +13,18 @@ char *_strncat(char *dest, char *src, int n)
 	int b;
 
 	a = 0;
-	while (dest[a] != 0)
+	while (dest[a] != '\0')
 	{
 		a++;
 	}
 	b = 0;
-	while (src[b] != 0 && b < n)
+	while (src[b] != '\0' && b < n)
 	{
 		dest[a] = src[b];
 		a++;
 		b++;
 	}
+	dest[a] = '\0';
 
 	return (dest);
 }
