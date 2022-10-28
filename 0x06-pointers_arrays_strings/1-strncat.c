7cde205bf33e15e16a0790a96e2d4e3dfb4e23d4
king-luminous <albashir97@gmail.comgit config --global user.email albashir97@gmail.com>
2022-10-28 04:16:56 -0700

    Task 01

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,8 +2,8 @@
 
 /**
  * _strncat - joins two strings with n bytes
- * @dest: copy to
- * @src: copy from
+ * @dest: copy input to
+ * @src: copy input from
  * @n: number of char to be copied
  * Return: dest
  */
@@ -13,18 +13,17 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	i = 0;
-	while (dest[i] != '\0')
+	while (dest[i] != 0)
 	{
 		i++;
 	}
 	j = 0;
-	while (j < n && src[j] != '\0')
+	while (src[j] != 0 && j < n)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	dest[i] = '\0';
 
 	return (dest);
 }
