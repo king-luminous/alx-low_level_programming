a1684f5800c24d49caca67f635ebd7439839311b
king-luminous <albashir97@gmail.comgit config --global user.email albashir97@gmail.com>
2022-10-31 16:58:04 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for set of bytes
+ * @s: pointer
+ * @accept: pointer
+ * Return: a pointer that matches to and from byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
