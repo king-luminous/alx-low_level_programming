2b3a5641e07e182dfdd10ae2a824fc6ab9440846
king-luminous <albashir97@gmail.comgit config --global user.email albashir97@gmail.com>
2022-10-31 15:59:59 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character
+ * @s: the memory
+ * @c: constant byte
+ *
+ * Return: a pointer in the found character
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+
+	return ('\0');
+}
