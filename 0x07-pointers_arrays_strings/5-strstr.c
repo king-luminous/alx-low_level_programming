899003209ac7c7e6e3c9568b66e63210e5b726e1
king-luminous <albashir97@gmail.comgit config --global user.email albashir97@gmail.com>
2022-10-31 17:22:44 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,9 +12,9 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
-	for (i = 0, haystack[i] != '\0';)
+	for (i = 0; haystack[i] != '\0';)
 	{
-		for (j = 0, needle[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (need[i] != haystack[i + j])
 			{
