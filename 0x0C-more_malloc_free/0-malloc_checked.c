083f09f08e89433b4c46deb57f905b7290c5c100
king-luminous <albashir97@gmail.comgit config --global user.email albashir97@gmail.com>
2022-11-10 13:39:39 -0800

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib>
+#include <stdlib.h>
 
 /**
  * malloc_checked - allocate memory to a pointer
@@ -12,6 +12,8 @@ void *malloc_checked(unsigned int b)
 	void *p = malloc(b);
 
 	if (p == NULL)
+
 		exit(98);
+
 	return (p);
 }
