ad07f7e676dd17bd1273c80e4e2f5d281e2ee89d
king-luminous <albashir97@gmail.comgit config --global user.email albashir97@gmail.com>
2022-11-15 06:56:39 -0800

    Task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MARCO_H
+#define OBJECT_LIKE_MARCO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Desc: Header file that defines a macro named SIZE.
+ *
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+this is not an empty readme file it contains the initial lectures on preprocessor
