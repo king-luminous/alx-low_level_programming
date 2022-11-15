c9a125ceb779652a161878e55fe57398a7dfc8f2
king-luminous <albashir97@gmail.comgit config --global user.email albashir97@gmail.com>
2022-11-15 07:50:54 -0800

    Task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,7 +3,7 @@
 
 /**
  * File: 3-function-like_marco.h
- * Desc: Header file that defines a function like marco
+ * Desc: Header file that defines a function like marco ABS(x) that compute x.
  */
 #define ABS(x) (((x) < 0) ? (x) * (-1) : (x))
 #endif
