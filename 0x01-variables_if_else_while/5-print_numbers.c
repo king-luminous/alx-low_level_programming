ddff57f9504e845b4aa076a80fd8eaa4c5871d8d
king-luminous <albashir97@gmail.com>
2022-12-13 12:58:49 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i  < 10; i++)
+	{
+		putchar("%d", i);
+		putchar('\n');
+	}
+	return (0);
+}
