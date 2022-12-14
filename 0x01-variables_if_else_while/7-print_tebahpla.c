352b67cdae418fbc4ebadf4851633125a77234c0
king-luminous <albashir97@gmail.com>
2022-12-14 13:31:44 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'z'; lc <= 'a'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
