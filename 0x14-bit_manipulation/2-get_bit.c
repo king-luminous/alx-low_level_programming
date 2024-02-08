128274bdd73386d53c3242171d0aded54cdd9df7
king-luminous <albashir97@gmail.com>
2024-02-08 13:05:05 -0800

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,17 +11,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsingned int i;
-
-	if (n == 0 && index < 64)
-		return (0);
-	for (i = 0, i <= 63; n >>= 1, i++)
-	{
-		if (index == 1)
-		{
-			return (n & 1);
-		}
-	}
-	return (-1);
+	if (index > 63)
+		return (-1);
+	return ((n >> index) & 1);
 }
-
