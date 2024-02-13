5c9c1538ee98a7c9fd979ca4f3603e6d43321c51
king-luminous <albashir97@gmail.com>
2024-02-13 07:42:43 -0800

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,19 +11,24 @@
 
 int append_text_to_file(const char *filename, char *text_content);
 {
-	int o, w, len = 0;
+	int fd, _write;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content != NULL)
+	fd = open(fulename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		close(fd);
+		return (1);
 	}
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-	if (o == -1 || w == -1)
+	_write = write(fd, text_content, strlen(text_content));
+	if (_write == -1 || _write != strlen(text_content))
+	{
+		close(fd);
 		return (-1);
-	close(0);
-	return (1);
+	}
+	close(fd);
+	return (-1);
 }
