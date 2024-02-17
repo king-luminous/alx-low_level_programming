6dd898de3e8644853d29b30f42cf2ee1e8ad8ffd
king-luminous <albashir97@gmail.com>
2024-02-17 07:47:05 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Return: always success
+ */
+
+int main(void)
+{
+	int d;
+	char sixteen;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for (sixteen = 'a'; sixteen <= 'f'; sixteen++)
+		putchar(sixten);
+	putchar('\n');
+
+	return (0);
+}
