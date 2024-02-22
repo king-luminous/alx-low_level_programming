6fdba0e236620b520d2b3d521bead05ec6a49526
king-luminous <albashir97@gmail.com>
2024-02-22 03:35:49 -0800

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int a, b;
 
-	for (a = '0'; a < '9' a++)
+	for (a = 0; a < 9 a++)
 	{
-		for (b = a + 1; b <= '9'; b++)
+		for (b = a + 1; b <= 9; b++)
 		{
 			if (a != b)
 			{
