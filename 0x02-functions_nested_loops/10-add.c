bdc4384f8d8b75d2d6314b65f3a17f695d167c49
king-luminous <albashir97@gmail.com>
2024-02-24 03:01:04 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two number
+ * @digit1: first integer
+ * @digit2: second integer
+ * Return: return always success
+ */
+
+int add(int digit1, int digit2);
+{
+
+	return (digit1 + digit2);
+}
